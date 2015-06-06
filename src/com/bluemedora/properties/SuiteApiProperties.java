4e4076a03c12d9219b3910f22e340a1b6fa15602
Mark Arbogast <mark.arbogast@bluemedora.com>
2015-06-05 21:02:13 -0400

    Add SuiteApiProperties and SuiteApiPropertiesTest.

--- src/com/bluemedora/properties/SuiteApiProperties.java
+++ src/com/bluemedora/properties/SuiteApiProperties.java
@@ -1,5 +1,58 @@
 package com.bluemedora.properties;
 
+import com.bluemedora.properties.exceptions.FailedToClosePropertiesFileException;
+import com.bluemedora.properties.exceptions.FailedToLoadPropertiesFileException;
+import com.bluemedora.properties.exceptions.PropertyNotFoundException;
+
 public class SuiteApiProperties
 {
+    public static final String HOST     = "host";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    private final PropertiesFile propertiesFile;
+
+    public SuiteApiProperties(PropertiesFile propertiesFile)
+    {
+        this.propertiesFile = propertiesFile;
+    }
+
+    public String getHost()
+    {
+        try {
+            return this.propertiesFile.getProperty(HOST);
+        } catch (PropertyNotFoundException e) {
+            return "";
+        } catch (FailedToLoadPropertiesFileException e) {
+            return "";
+        } catch (FailedToClosePropertiesFileException e) {
+            return "";
+        }
+    }
+
+    public String getUsername()
+    {
+        try {
+            return this.propertiesFile.getProperty(USERNAME);
+        } catch (PropertyNotFoundException e) {
+            return "";
+        } catch (FailedToLoadPropertiesFileException e) {
+            return "";
+        } catch (FailedToClosePropertiesFileException e) {
+            return "";
+        }
+    }
+
+    public String getPassword()
+    {
+        try {
+            return this.propertiesFile.getProperty(PASSWORD);
+        } catch (PropertyNotFoundException e) {
+            return "";
+        } catch (FailedToLoadPropertiesFileException e) {
+            return "";
+        } catch (FailedToClosePropertiesFileException e) {
+            return "";
+        }
+    }
 }
--- test/com/bluemedora/properties/SuiteApiPropertiesTest.java
+++ test/com/bluemedora/properties/SuiteApiPropertiesTest.java
@@ -1,12 +1,128 @@
 package com.bluemedora.properties;
 
+import com.bluemedora.properties.exceptions.FailedToClosePropertiesFileException;
+import com.bluemedora.properties.exceptions.FailedToLoadPropertiesFileException;
+import com.bluemedora.properties.exceptions.PropertyNotFoundException;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 public class SuiteApiPropertiesTest
 {
+    private SuiteApiProperties target;
+    private PropertiesFile     mockPropertiesFile;
+
+    private static final String MOCK_VALUE    = "mockValue";
+
     @Before
     public void setUp()
     {
+        this.mockPropertiesFile = mock(PropertiesFile.class);
+        this.target = new SuiteApiProperties(this.mockPropertiesFile);
+    }
 
+    @Test
+    public void getHost()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.HOST)).thenReturn(MOCK_VALUE);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        }
+
+        assertEquals(MOCK_VALUE, this.target.getHost());
+    }
+
+    @Test
+    public void getHost_returnsEmptyStringForPropertyNotFoundException()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.HOST)).thenThrow(PropertyNotFoundException.class);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        }
+
+        assertEquals("", this.target.getHost());
+    }
+
+    @Test
+    public void getHost_returnsEmptyStringForFailedToLoadPropertiesFileException()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.HOST)).thenThrow(FailedToLoadPropertiesFileException.class);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        }
+
+        assertEquals("", this.target.getHost());
+    }
+
+    @Test
+    public void getHost_returnsEmptyStringForFailedToClosePropertiesFileException()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.HOST)).thenThrow(FailedToClosePropertiesFileException.class);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.HOST);
+        }
+
+        assertEquals("", this.target.getHost());
+    }
+
+    @Test
+    public void getUsername()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.USERNAME)).thenReturn(MOCK_VALUE);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.USERNAME);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.USERNAME);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.USERNAME);
+        }
+
+        assertEquals(MOCK_VALUE, this.target.getUsername());
+    }
+
+    @Test
+    public void getPassword()
+    {
+        try {
+            when(this.mockPropertiesFile.getProperty(SuiteApiProperties.PASSWORD)).thenReturn(MOCK_VALUE);
+        } catch (PropertyNotFoundException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.PASSWORD);
+        } catch (FailedToLoadPropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.PASSWORD);
+        } catch (FailedToClosePropertiesFileException e) {
+            failDueToUnexpectedException(e, SuiteApiProperties.PASSWORD);
+        }
+
+        assertEquals(MOCK_VALUE, this.target.getPassword());
+    }
+
+    private void failDueToUnexpectedException(Exception e, String property) {
+        fail("Unexpected " + e.getClass() + " when getting " + property);
     }
 }
+
