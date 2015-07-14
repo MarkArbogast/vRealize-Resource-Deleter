62e88cad4ccb456b4c4913b057ef4e45da40ac0a
Mark Arbogast <mark.arbogast@bluemedora.com>
2015-07-14 18:59:09 -0400

    Stashing ruby changes.

--- delete_resource.rb
+++ delete_resource.rb
@@ -1,15 +1,23 @@
+require 'rest-client'
 require 'io/console'
 require 'net/http'
 
-print "vRealize Hostname: "
-hostname = gets.chomp
+connection = nil
+while(!(connection = get_connection()))
 
-print "vRealize Username: "
-username = gets.chomp
+def get_connection
+  print "vRealize Hostname: "
+  hostname = gets.chomp
 
-print "vRealize Password: "
-password = STDIN.noecho(&:gets).chomp
+  print "vRealize Username: "
+  username = gets.chomp
 
-puts
+  print "vRealize Password: "
+  password = STDIN.noecho(&:gets).chomp
+
+  puts
+
+  r = Request.new({:user => username, :password => password})
+end
 
 
